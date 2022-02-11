c684236a0d65551501ddf653fef6c0f12aa64086
Konrad Dzwinel <konrad@duckduckgo.com>
2022-02-11 16:11:05 +0100

    Tests for fingerprinting protections (#28)
    * WIP - fingerprinting protection tests
    
    * Enable features in config
    
    * add siteURL and exceptions
    
    * Fix exception format
    
    * Update BateryManager proto
    
    * update init proto
    
    * Update readme
    
    * Add support for temporary storage, fix descriptions
    
    * fix temp storage init
    
    * Improve docs/comments after review

---
+++ fingerprinting-protections/README.md
@@ -0,0 +1,43 @@
+# Fingerprinting Protection Tests
+
+Privacy Feature: https://app.asana.com/0/1198207348643509/1199583771657237/f
+
+## Goals
+
+This set of tests verifies implementation of fingerprinting protections. In particular it focuses on verifying that:
+
+- exceptions from configuration file are respected,
+- values of properites are modified correctly.
+
+## Structure
+
+Test suite specific files:
+
+- `init.js` - set of API mocks that should be loaded into the page before protections are initialized. This allows us to normalize default/unprotected values.
+
+Test suite specific fields:
+
+- `siteURL` - string - currently loaded website's URL (as seen in the URL bar) 
+- `property` - string - JavaScript code extracting value of given property/API. Note that in some cases this code is async, please wait for returned promise to resolve.
+- `expectPropertyValue` - string - expected value of the property
+
+## Pseudo-code implementation
+
+```
+for $testSet in test.json
+  loadReferenceConfig('config_reference.json')
+
+    for $test in $testSet
+        if $test.exceptPlatforms includes 'current-platform'
+            skip
+
+        $page = createPage(
+            siteURL = $test.siteURL,
+        )
+
+        $page.load('init.js')
+
+        $value = $page.eval($test.property)
+
+        expect($value.toSting()).toBe($test.expectPropertyValue)
+```
---
+++ fingerprinting-protections/init.js
@@ -0,0 +1,32 @@
+function init(window) {
+    // values of properties are being set by this script on purpose to be 'outlier' values,
+    // so the test can verify protection in a platform agnostic way
+
+    const BatteryManager = function BatteryManager () {}
+    Object.defineProperty(BatteryManager.prototype, 'charging', { get: () => false, configurable: true });
+    Object.defineProperty(BatteryManager.prototype, 'chargingTime', { get: () => 12345, configurable: true });
+    Object.defineProperty(BatteryManager.prototype, 'dischargingTime', { get: () => 12345, configurable: true });
+    
+    window.BatteryManager = BatteryManager;
+    window.navigator.getBattery = () => Promise.resolve(new BatteryManager());
+
+    Object.defineProperty(window.Navigator.prototype, 'deviceMemory', { get: () => 12345, configurable: true });
+    Object.defineProperty(window.Navigator.prototype, 'hardwareConcurrency', { get: () => 12345, configurable: true });
+
+    Object.defineProperty(window.Screen.prototype, 'availTop', { get: () => 12345, configurable: true });
+    Object.defineProperty(window.Screen.prototype, 'availLeft', { get: () => 12345, configurable: true });
+    Object.defineProperty(window.Screen.prototype, 'availWidth', { get: () => 12345, configurable: true });
+    Object.defineProperty(window.Screen.prototype, 'availHeight', { get: () => 12345, configurable: true });
+    Object.defineProperty(window.Screen.prototype, 'colorDepth', { get: () => 12345, configurable: true });
+    Object.defineProperty(window.Screen.prototype, 'pixelDepth', { get: () => 12345, configurable: true });
+
+    window.navigator.webkitTemporaryStorage = {
+        queryUsageAndQuota: callback => {
+            callback(0, 9999999999)
+        }
+    };
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = init
+}
